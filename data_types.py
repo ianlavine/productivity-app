cec3cbf57ba178a874f6131cf6dec7b21785d851
Stephanie Lum <stephy.lum87@gmail.com>
2021-05-17 22:13:39 -0400

    update data_types.py

---
+++ data_types.py
@@ -0,0 +1,24 @@
+""" Ideal Day - data_types.py
+Contains classes and data structures for events in schedule
+"""
+# Constants
+PRIORITIES = {"!", "!!", "!!!"}
+
+class Event:
+    """Python class representing an event/activity for a specific day
+    
+    Instance Attributes:
+    - title: name of event
+    - description: further details about the event
+    - priority: how important this event for the day
+
+    Representation invariants:
+    - self.priority in PRIORITIES
+    """
+    title: str
+    description: str
+    priority: str
+
+    def init(self) -> None:
+        """initilize an event instance"""
+
